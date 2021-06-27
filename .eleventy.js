22e60d5116c00e40ac46308747f7e41449018138
Raul Piraces Alastuey <raul.piraces@gmail.com>
2021-06-27 19:18:15 +0000

    Several fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const swBuild = require("./src/builder");
 
-module.exports = function (eleventyConfig) {
+module.exports = function (eleventyConfig, options = {}) {
   eleventyConfig.on('afterBuild', () => {
-    swBuild(options, eleventyConfig.dir.output).then((res) => console.log(res));
+    const Eleventy = require("@11ty/eleventy/src/Eleventy");
+    const outputDir = new Eleventy().outputDir;
+    swBuild(options, outputDir).then((res) => console.log(res));
   });
 };
